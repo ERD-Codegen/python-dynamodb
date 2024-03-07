73628e1843047060e05bcbf520b0b36ae8309ac0
ldw <ldw3097@gmail.com>
2024-03-07 11:44:17 +0900

    test: Add tests for user and fixed user

--- serverless/src/user.py
+++ serverless/src/user.py
@@ -6,126 +6,119 @@ import jwt
 import bcrypt
 from src.util import *
 
-dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2')
+dynamodb = boto3.resource("dynamodb", region_name="ap-northeast-2")
 users_table = dynamodb.Table("dev-users")
 
 
 # create user
 def create_user(event, context):
-
+    body = event["body"]
     # input validation
-    if "user" not in event:
+    if "user" not in body:
         raise Exception("User must be specified.", 422)
-    user = event['user']
-    if 'username' not in user:
+    user = body["user"]
+    if "username" not in user:
         logging.error("Validation Failed")
         raise Exception("Username must be specified.", 422)
-    if 'email' not in user:
+    if "email" not in user:
         logging.error("Validation Failed")
         raise Exception("Email must be specified.", 422)
-    if 'password' not in user:
+    if "password" not in user:
         logging.error("Validation Failed")
         raise Exception("Password must be specified.", 422)
 
     # Verify username is not taken
-    user_exists = get_user_by_username(user['username'])
-    if "Item" in user_exists:
+    user_exists = get_user_by_username(user["username"])
+    if user_exists:
         logging.error("Validation Failed")
         raise Exception(f"Username already taken: {user['username']}", 422)
 
     # Verify email is not taken
-    email_exists = get_user_by_email(user['email'])
+    email_exists = get_user_by_email(user["email"])
     if email_exists["Count"]:
         logging.error("Validation Failed")
         raise Exception(f"Email already taken: {user['email']}", 422)
-    
+
     # Add new entry to usersTable
     encryptedPassword = bcrypt.hashpw(user["password"].encode(), bcrypt.gensalt())
     item = {
-        'username': user['username'],
-        'email': user['email'],
-        'password': encryptedPassword,
+        "username": user["username"],
+        "email": user["email"],
+        "password": encryptedPassword,
     }
     users_table.put_item(Item=item)
-    
+
     # Return user info with jwt token
     user = {
-        'email': user['email'],
-        'token': mint_token(user['username']),
-        'username': user['username'],
-        'bio': '',
-        'image': ''
+        "email": user["email"],
+        "token": mint_token(user["username"]),
+        "username": user["username"],
+        "bio": "",
+        "image": "",
     }
+
     return envelop({"user": user})
 
 
 def mint_token(a_username):
-    payload = {
-        'username': a_username,
-        'exp': datetime.utcnow() + timedelta(days=2)
-    }
+    payload = {"username": a_username, "exp": datetime.utcnow() + timedelta(days=2)}
     jwt_token = jwt.encode(payload, JWT_SECRET_KEY, JWT_ALGORITHM)
     return jwt_token
 
 
 def get_user_by_username(username):
-    print(f"REQ:{username}")
     try:
-        response = users_table.get_item(
-            Key={
-                'username': username
-            }
-        )
+        response = users_table.get_item(Key={"username": username})["Item"]
     except Exception as e:
         response = None
-    print(f"RESP:{response}")
     return response
 
 
 def get_user_by_email(a_email):
     response = users_table.query(
-        IndexName='email',
-        KeyConditionExpression='email= :email',
+        IndexName="email",
+        KeyConditionExpression="email= :email",
         ExpressionAttributeValues={
-            ':email': a_email,
+            ":email": a_email,
         },
-        Select='ALL_ATTRIBUTES',
+        Select="ALL_ATTRIBUTES",
     )
-    print(f"EMAIL:{response}")
     return response
 
 
 # login user
 def login_user(event, context):
-    user = event['user']
-    
+    user = event["user"]
+
     # input validation
     if not user:
         logging.error("Validation Failed")
         raise Exception("User must be specified.", 422)
-    if 'email' not in user:
+    if "email" not in user:
         logging.error("Validation Failed")
         raise Exception("Email must be specified.", 422)
-    if 'password' not in user:
+    if "password" not in user:
         logging.error("Validation Failed")
         raise Exception("Password must be specified.", 422)
 
     # Get user with this email
-    user_with_this_email = get_user_by_email(user['email'])
-    if user_with_this_email['Count'] != 1:
+    user_with_this_email = get_user_by_email(user["email"])
+    if user_with_this_email["Count"] != 1:
         raise Exception(f"Email not fount: {user['email']}.", 422)
-        
+
     # Check password
-    if not bcrypt.checkpw(user["password"].encode(), user_with_this_email['Items'][0]['password'].encode()):
+    if not bcrypt.checkpw(
+        user["password"].encode(), bytes(user_with_this_email["Items"][0]["password"])
+    ):
         raise Exception("Wrong password.", 422)
 
     # Return user with jwt token
     authenticated_user = {
-        'email': user['email'],
-        'token': mint_token(user_with_this_email['Items'][0]['username']),
-        'username': user_with_this_email['Items'][0]['username'],
-        'bio': user_with_this_email['Items'][0].get("bio", ""),
-        'image': user_with_this_email['Items'][0].get("image", "")
+        "email": user["email"],
+        "token": mint_token(user_with_this_email["Items"][0]["username"]),
+        "username": user_with_this_email["Items"][0]["username"],
+        "bio": user_with_this_email["Items"][0].get("bio", ""),
+        "image": user_with_this_email["Items"][0].get("image", ""),
     }
 
     return envelop({"user": authenticated_user})
@@ -134,15 +127,14 @@ def login_user(event, context):
 # get user
 def get_user(event, context):
     authenticated_user = authenticate_and_get_user(event)
-    if not authenticated_user:
-        raise Exception('Token not present or invalid.', 422)
-
+    if authenticated_user is None:
+        raise Exception("Token not present or invalid.", 422)
     user = {
-        'email': authenticated_user['email'],
-        'token': get_token_from_event(event),
-        'username': authenticated_user['username'],
-        'bio': authenticated_user.get("bio", ""),
-        'image':  authenticated_user.get("image", "")
+        "email": authenticated_user["email"],
+        "token": get_token_from_event(event),
+        "username": authenticated_user["username"],
+        "bio": authenticated_user.get("bio", ""),
+        "image": authenticated_user.get("image", ""),
     }
 
     return envelop({"user": user})
@@ -151,159 +143,137 @@ def get_user(event, context):
 def update_user(event, context):
     authenticated_user = authenticate_and_get_user(event)
     if not authenticated_user:
-        raise Exception('Token not present or invalid.', 422)
-
-    user = event["user"]
-    if not user:
-        logging.error("Validation Failed")
-        raise Exception("User must be specified.", 422)
-
+        raise Exception("Token not present or invalid.", 422)
     updated_user = authenticated_user
 
-    if user['email']:
+    if "user" not in event["body"]:
+        logging.error("Validation Failed")
+        raise Exception("User must be specified.", 422)
+    user = event["body"]["user"]
+
+    if "email" in user:
         # Verify email is not taken
-        user_with_this_email = get_user_by_email(user['email'])
-        if user_with_this_email['Count'] != 0:
+        user_with_this_email = get_user_by_email(user["email"])
+        if user_with_this_email["Count"] != 0:
             return Exception(f"Email already taken: {user['email']}", 422)
-        updated_user['email'] = user['email']
+        updated_user["email"] = user["email"]
 
-    if user['password']:
-        updated_user['password'] = bcrypt.hashpw(user["password"].encode(), bcrypt.gensalt())
+    if "password" in user:
+        updated_user["password"] = bcrypt.hashpw(
+            user["password"].encode(), bcrypt.gensalt()
+        )
 
-    if user['image']:
-        updated_user['image'] = user['image']
+    if "image" in user:
+        updated_user["image"] = user["image"]
 
-    if user['bio']:
-        updated_user['bio'] = user['bio']
-
-    print(f"UPDATE: {updated_user}")
-
+    if "bio" in user:
+        updated_user["bio"] = user["bio"]
 
     users_table.put_item(Item=updated_user)
 
-    del updated_user['password']
-    updated_user['token'] = get_token_from_event(event)
+    del updated_user["password"]
+    updated_user["token"] = get_token_from_event(event)
 
     return envelop({"user": updated_user})
 
 
 def get_profile(event, context):
-    username = event['pathParameters']['username']
+    username = event["pathParameters"]["username"]
     authenticated_user = authenticate_and_get_user(event)
-    profile = get_profile_by_username(username, authenticated_user);
-    print(f"PROFILE: {profile}")
-    if not profile:
-        raise Exception(f"User not found: ${username}", 422)
+    profile = get_profile_by_username(username, authenticated_user)
 
-    response = {
-        "statusCode": 200,
-        "headers": {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': 'true'
-        },
-        "profile": profile
-    }
-    return response
+    if profile is None:
+        raise Exception(f"User not found: {username}", 422)
+
+    return envelop({"profile": profile})
 
 
 def follow(event, context):
-    authenticated_user = authenticate_and_get_user(event, context)
-    if not authenticated_user:
-        raise Exception('Token not present or invalid.', 422)
-    username = event['pathParameters']['username']
-    user = (get_user_by_username(username))['Item']
-    should_follow = (not event['httpMethod'] == 'DELETE')
+    authenticated_user = authenticate_and_get_user(event)
+    if authenticated_user is None:
+        raise Exception("Token not present or invalid.", 422)
+
+    username = event["pathParameters"]["username"]
+    user = get_user_by_username(username)
+    should_follow = event["httpMethod"] != "DELETE"
 
     # Update "followers" field on followed user
     if should_follow:
-        if user['followers'] and authenticated_user['username'] not in user['followers']:
-            pass
-            user['followers'].append(authenticated_user['username'])
+        if (
+            "followers" in user
+            and authenticated_user["username"] not in user["followers"]
+        ):
+            user["followers"].append(authenticated_user["username"])
         else:
-            user['followers'] = [authenticated_user['username']]
+            user.setdefault("followers", [authenticated_user["username"]])
     else:
-        if user['followers'] and authenticated_user['username'] in user['followers']:
-            # create new list of follower except authenticated user
-            follow_result = filter(lambda x: x != authenticated_user['username'], user['followers'])
-            user['followers'] = list(follow_result)
+        if "followers" in user and authenticated_user["username"] in user["followers"]:
+            user["followers"].remove(authenticated_user["username"])
 
             # delete followers if list is empty
-            if not len(user['followers']):
-                print("In Delete condition")
-                del user['followers']
+            if len(user["followers"]) == 0:
+                del user["followers"]
 
-    table = dynamodb.Table('dev-users')
-    table.put_item(Item=user)
+    users_table.put_item(Item=user)
 
     # Update "following" field on follower user
     if should_follow:
-        if authenticated_user['following'] and authenticated_user['following'] not in username:
-            authenticated_user['following'].append(username)
+        if (
+            "following" in authenticated_user
+            and username not in authenticated_user["following"]
+        ):
+            authenticated_user["following"].append(username)
         else:
-            authenticated_user['following'] = [username]
+            authenticated_user.setdefault("following", [username])
     else:
-        if authenticated_user['following'] and authenticated_user['following'] in username:
-            # create new list of following except username
-            result = filter(lambda x: x != username, authenticated_user['following'])
-            authenticated_user['following'] = list(result)
+        if (
+            "following" in authenticated_user
+            and username in authenticated_user["following"]
+        ):
+            authenticated_user["following"].remove(username)
 
             # delete following if list is empty
-            if not len(authenticated_user['following']):
-                del authenticated_user['following']
+            if len(authenticated_user["following"]) == 0:
+                del authenticated_user["following"]
 
-    table.put_item(Item=authenticated_user)
+    users_table.put_item(Item=authenticated_user)
 
     profile = {
-        'username': username,
-        'bio': user['bio'] if user['bio'] else '',
-        'image': user['image'] if user['bio'] else '',
-        'following': should_follow
+        "username": username,
+        "bio": user.get("bio", ""),
+        "image": user.get("image", ""),
+        "following": should_follow,
     }
 
-    response = {
-        "statusCode": 200,
-        "headers": {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': 'true'
-        },
-        "profile": profile
-    }
-
-    return response
+    return envelop({"profile": profile})
 
 
-# create followed users
 def get_followed_users(a_username):
-    table = dynamodb.Table('dev-users')
-    user = table.get_item(
-        Key={
-            'username': a_username
-        }
-    )['Item']
-
-    return user['following'] or user['following']== []
+    user = users_table.get_item(Key={"username": a_username})
+    if "Item" not in user:
+        raise Exception("user not found", 422)
+    user = user["Item"]
+    return user.get("following", [])
 
 
 def get_token_from_event(event):
-    return event['headers']['authorization'].replace('Token', '')
+    return event["headers"]["Authorization"].split(" ")[1]
 
 
 def get_profile_by_username(a_username, a_authenticated_user):
-    user = get_user_by_username(a_username)['Item']
-    print(f"PROFILE USER: {user}")
-    if not user:
+    user = get_user_by_username(a_username)
+    if user is None:
         return None
-
     profile = {
-        'username': user['username'],
-        'bio': user.get("bio", ""),
-        'image': user.get("image", ""),
-        'following': False,
+        "username": user["username"],
+        "bio": user.get("bio", ""),
+        "image": user.get("image", ""),
+        "following": False,
     }
 
     # If user is authenticated, set following bit
-    if user['followers'] and a_authenticated_user:
-        profile['following'] = user['followers'] in a_authenticated_user['username']
+    if "followers" in user and a_authenticated_user:
+        profile["following"] = a_authenticated_user["username"] in user["followers"]
 
     return profile
 
@@ -311,8 +281,8 @@ def get_profile_by_username(a_username, a_authenticated_user):
 def authenticate_and_get_user(event):
     try:
         token = get_token_from_event(event)
-        decoded = jwt.decode(token, JWT_SECRET_KEY, JWT_ALGORITHM),
-        username = decoded.username,
+        decoded = jwt.decode(token, JWT_SECRET_KEY, JWT_ALGORITHM)
+        username = decoded["username"]
         authenticated_user = get_user_by_username(username)
         return authenticated_user
     except Exception as e:
--- serverless/test/test_user.py
+++ serverless/test/test_user.py
@@ -30,14 +30,156 @@ def users_table(dynamodb_client):
     yield table
 
 
-def test_create_user(users_table, dynamodb_client):
-    user1 = {
+@pytest.fixture
+def user1():
+    return {
         "username": "john doe",
         "email": "johndoe@gmail.com",
         "password": "password123",
     }
 
-    event = {"user": user1}
+
+@pytest.fixture
+def user2():
+    return {
+        "username": "kim doe",
+        "email": "kimdoe@gmail.com",
+        "password": "password321",
+    }
+
+
+def test_create_user(users_table, user1):
+
+    eventbody = {"user": user1}
+    event = {"body": eventbody}
     ret = user.create_user(event, {})
     assert ret["body"]["user"]["username"] == "john doe"
     print(ret)
+
+
+def test_login_user(users_table, user1):
+    eventbody = {"user": user1}
+    event = {"body": eventbody}
+    ret = user.create_user(event, {})
+
+    event = {"user": {"email": "johndoe@gmail.com", "password": "password123"}}
+    ret = user.login_user(event, {})
+    assert ret["statusCode"] == 200
+    assert ret["body"]["user"]["username"] == "john doe"
+    assert ret["body"]["user"]["email"] == "johndoe@gmail.com"
+    assert "token" in ret["body"]["user"]
+
+
+def test_invalid_login_user(users_table, user1):
+    eventbody = {"user": user1}
+    event = {"body": eventbody}
+    ret = user.create_user(event, {})
+
+    event = {"user": {"email": "johndoe@gmail.com", "password": "password1234"}}
+    with pytest.raises(Exception, match="Wrong password"):
+        user.login_user(event, {})
+
+
+def test_get_user(users_table, user1):
+    eventbody = {"user": user1}
+    event = {"body": eventbody}
+    ret = user.create_user(event, {})
+
+    event2 = {"headers": {"Authorization": "Bearer " + ret["body"]["user"]["token"]}}
+    ret = user.get_user(event2, {})
+    assert ret["statusCode"] == 200
+    assert ret["body"]["user"]["username"] == "john doe"
+
+
+def test_get_user_invalid_token(users_table, user1):
+    eventbody = {"user": user1}
+    event = {"body": eventbody}
+    ret = user.create_user(event, {})
+
+    event2 = {
+        "headers": {
+            "Authorization": "Bearer " + ret["body"]["user"]["token"] + "invalid"
+        }
+    }
+    with pytest.raises(Exception, match="Token not present or invalid."):
+        ret = user.get_user(event2, {})
+
+
+def test_update_user(users_table, user1):
+    eventbody = {"user": user1}
+    event = {"body": eventbody}
+    created = user.create_user(event, {})
+
+    event2 = {
+        "headers": {"Authorization": "Bearer " + created["body"]["user"]["token"]},
+        "body": {
+            "user": {
+                "username": "john doe",
+                "email": "altered@gmail.com",
+                "password": "altered123",
+                "bio": "I love python and lambda!",
+                "image": "https://image.com",
+            }
+        },
+    }
+
+    user.update_user(event2, {})
+    event3 = {
+        "headers": {"Authorization": "Bearer " + created["body"]["user"]["token"]}
+    }
+    ret = user.get_user(event3, {})
+    assert ret["statusCode"] == 200
+    assert ret["body"]["user"]["email"] == "altered@gmail.com"
+    assert ret["body"]["user"]["bio"] == "I love python and lambda!"
+    assert ret["body"]["user"]["image"] == "https://image.com"
+
+
+def test_get_profile(users_table, user1):
+    eventbody = {"user": user1}
+    event = {"body": eventbody}
+    created = user.create_user(event, {})
+
+    event2 = {"pathParameters": {"username": "john doe"}}
+    ret = user.get_profile(event2, {})
+    assert ret["statusCode"] == 200
+    assert ret["body"]["profile"]["username"] == "john doe"
+    assert ret["body"]["profile"]["following"] == False
+
+
+def test_get_profile_nonexisting(users_table, user1):
+    eventbody = {"user": user1}
+    event = {"body": eventbody}
+    created = user.create_user(event, {})
+
+    event2 = {"pathParameters": {"username": "nonexisting"}}
+    with pytest.raises(Exception, match="User not found: nonexisting"):
+        ret = user.get_profile(event2, {})
+
+
+def test_follow_user(users_table, user1, user2):
+    eventbody = {"user": user1}
+    event = {"body": eventbody}
+    created1 = user.create_user(event, {})
+
+    eventbody = {"user": user2}
+    event = {"body": eventbody}
+    created2 = user.create_user(event, {})
+
+    event2 = {
+        "headers": {"Authorization": "Bearer " + created2["body"]["user"]["token"]},
+        "httpMethod": "POST",
+        "pathParameters": {"username": "john doe"},
+    }
+    ret = user.follow(event2, {})
+    assert ret["statusCode"] == 200
+    assert ret["body"]["profile"]["username"] == "john doe"
+
+    event3 = {
+        "headers": {"Authorization": "Bearer " + created2["body"]["user"]["token"]},
+        "httpMethod": "GET",
+        "pathParameters": {"username": "john doe"},
+    }
+
+    profile = user.get_profile(event2, {})
+    assert profile["statusCode"] == 200
+    assert profile["body"]["profile"]["following"] == True
